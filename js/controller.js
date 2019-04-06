3bba5a14551bfcb6634b0d1a3e4ca0631e9da0da
plc <paul.l.christ@web.de>
2019-04-06 15:59:54 +0200

    implementing js logic

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,21 @@
-document.querySelector("[button]");
+/**
+ * creating eventlisteners for elements intended for user interaction
+ */
+var buttons = document.querySelectorAll("[id*=Button]");
+buttons.forEach(function(button) {
+  button.addEventListener("click", nextNote);
+});
 
-document.querySelector("#progress");
+var radioButtons = document.querySelectorAll("#violin, #bass");
+radioButtons.forEach(function() {});
+
+function progressBar() {
+  document.querySelector("#progress").value =
+    document.querySelector("#progress").value + 10;
+}
+
+function statistic() {}
+
+function nextNote() {
+  progressBar();
+}
